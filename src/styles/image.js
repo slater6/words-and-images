bb4370fc1c8986c7ab4c014bd05f49e3f2476bf9
slater6 <slaters@gmail.com>
2017-10-19 22:58:14 -0400

    Added some animation styling for Image

--- src/styles/image.js
+++ src/styles/image.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled,{keyframes} from 'styled-components'
 
 export const ImageWrapper = styled.section.attrs({
     display: props => (props.isVisible.length === 0) ? 'none' : 'flex'
@@ -13,8 +13,19 @@ export const ImageColumn = styled.section`
     align-items:center;
 `
 
+const activeImage = keyframes`
+    0% {
+        transform:opacity(0);
+    }
+
+
+    100% {
+        transform:opacity(1);
+    }
+`;
 
 export const ImageDisplayer = styled.img`
     border: 3px solid black;
     margin-bottom: 20px;
+    animation: ${activeImage} 100ms ease-in;
 `
